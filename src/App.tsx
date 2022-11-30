c96df83e00515e18c510f96d390e525ad038ca61
AnnSyh <ann.syharevskaya@gmail.com>
2022-11-30 14:54:26 +0300

    init react with typscript + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
